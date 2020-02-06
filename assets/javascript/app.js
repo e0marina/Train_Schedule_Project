d7c44724f23ac3a5d70e6cc1c21349a5717ca2b5
Emily M <30880308+e0marina@users.noreply.github.com>
2020-02-05 21:24:51 -0800

    added jumbotron, started table

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+//Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyCNYhZY7DWHF7pxQQJlAuEKOfOCbv55EuU",
+  authDomain: "emily-s-class-project.firebaseapp.com",
+  databaseURL: "https://emily-s-class-project.firebaseio.com",
+  projectId: "emily-s-class-project",
+  storageBucket: "emily-s-class-project.appspot.com",
+  messagingSenderId: "127915851584",
+  appId: "1:127915851584:web:32358ea6802aa9446104a5"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+//======================================================================
+//GLOBAL VARIABLES
+//======================================================================
+
+//======================================================================
+//FUNCTIONS
+//======================================================================
